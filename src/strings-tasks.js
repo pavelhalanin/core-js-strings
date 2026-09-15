f11895f46f1f59f2df3be38633427ae3743d15db
pavelhalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-09-15 19:07:33 +0300

    feat: 3 task is done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -57,8 +57,8 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return String(value1).concat(String(value2));
 }
 
 /**
