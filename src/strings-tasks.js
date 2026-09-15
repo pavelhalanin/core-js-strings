442dba3275a805e36ec9d48992d237aa066301d5
pavelhalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-09-15 21:42:40 +0300

    feat: 18 task is done

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -320,8 +320,11 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  return str
+    .toLowerCase()
+    .split('')
+    .filter((e) => 'eyuioa'.includes(e)).length;
 }
 
 /**
